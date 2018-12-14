62a8ec3f12ebc6561dfba14aad273f312f898a55
Theron Luhn <theron@luhn.com>
2018-12-14 14:31:41 -0800

    Add integration tests.

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import pytest
+from webtest import TestApp as _TestApp
+from pyramid.config import Configurator
 from pyramid_resource import Resource
 
 
@@ -81,3 +83,81 @@ def test_getattr():
     assert grandchild.foo == 'bar'
     assert grandchild.subfoo == 'subbar'
     assert grandchild.prop == 'myprop'
+
+
+# Integration tests
+
+
+@pytest.fixture
+def app():
+    class WidgetContainer(Resource):
+        def get_child(self, key):
+            try:
+                int_key = int(key)
+            except ValueError:
+                raise KeyError
+
+            if int_key not in range(10):
+                raise KeyError
+
+            return Widget, {'widget_id': int_key}
+
+    class Widget(Resource):
+        @property
+        def widget(self):
+            # Perhaps a DB lookup
+            return 'mywidget:{}'.format(self.widget_id)
+
+    class Root(Resource):
+        __children__ = {
+            'widget': WidgetContainer,
+        }
+
+    def list_widgets(context, request):
+        urls = []
+        for widget_id in [1, 2, 5]:
+            urls.append(request.resource_path(context[widget_id]))
+        return {
+            'widget_urls': urls,
+        }
+
+    def get_widget_id(context, request):
+        assert isinstance(context.widget_id, int)
+        return context.widget_id
+
+    def get_widget(context, request):
+        return context.widget
+
+    with Configurator(settings={}) as config:
+        config.set_root_factory(Root)
+        config.add_view(list_widgets, context=WidgetContainer, renderer='json')
+        config.add_view(get_widget, context=Widget, renderer='string')
+        config.add_view(get_widget_id, context=Widget, name='id',
+                        renderer='string')
+    return _TestApp(config.make_wsgi_app())
+
+
+def test_get_widget_id(app):
+    assert app.get('/widget/5/id').text == '5'
+
+
+def test_get_widget(app):
+    assert app.get('/widget/5/').text == 'mywidget:5'
+
+
+def test_get_widget_not_found(app):
+    assert app.get('/widget/15/', status=404)
+
+
+def test_get_widget_bad_id(app):
+    assert app.get('/widget/not-an-int/', status=404)
+
+
+def test_get_widget_list(app):
+    assert app.get('/widget/').json == {
+        'widget_urls': [
+            '/widget/1/',
+            '/widget/2/',
+            '/widget/5/',
+        ],
+    }
