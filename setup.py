02c85be703771fdea723dbc9e18aafa100888fa2
Theron Luhn <theron@luhn.com>
2021-01-09 09:37:04 -0800

    Update classifiers.

--- setup.py
+++ setup.py
@@ -4,6 +4,18 @@ VERSION = "0.3.0"
 CLASSIFIERS = [
     "Development Status :: 4 - Beta",
     "Framework :: Pyramid",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Programming Language :: Python",
 ]
 
 REQUIRES = ["pyramid>=1.7,<2.0"]
