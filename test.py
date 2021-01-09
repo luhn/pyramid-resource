a3cd78f0f9c6848d244f624e53d1836b37771a6d
Theron Luhn <theron@luhn.com>
2021-01-09 09:38:18 -0800

    Format test file.

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import pytest
-from webtest import TestApp as _TestApp
 from pyramid.config import Configurator
+from webtest import TestApp as _TestApp
+
 from pyramid_resource import Resource
 
 
@@ -15,18 +16,18 @@ def test_default_lookup():
 
     class MyResource(Resource):
         __children__ = {
-            'sub': SubResource,
+            "sub": SubResource,
         }
 
-    root = MyResource('request')
-    sub = root['sub']
+    root = MyResource("request")
+    sub = root["sub"]
     assert isinstance(sub, SubResource)
-    assert sub.request == 'request'
-    assert sub.__name__ == 'sub'
+    assert sub.request == "request"
+    assert sub.__name__ == "sub"
     assert sub.__parent__ is root
 
     with pytest.raises(KeyError):
-        root['sub2']
+        root["sub2"]
 
 
 def test_custom_lookup_subclass():
@@ -35,14 +36,14 @@ def test_custom_lookup_subclass():
 
     class MyResource(Resource):
         def get_child(self, key):
-            assert key == 'sub'
+            assert key == "sub"
             return SubResource
 
-    root = MyResource('request')
-    sub = root['sub']
+    root = MyResource("request")
+    sub = root["sub"]
     assert isinstance(sub, SubResource)
-    assert sub.request == 'request'
-    assert sub.__name__ == 'sub'
+    assert sub.request == "request"
+    assert sub.__name__ == "sub"
     assert sub.__parent__ is root
 
 
@@ -52,16 +53,16 @@ def test_custom_lookup_tuple():
 
     class MyResource(Resource):
         def get_child(self, key):
-            assert key == 'sub'
-            return SubResource, {'foo': 'bar'}
+            assert key == "sub"
+            return SubResource, {"foo": "bar"}
 
-    root = MyResource('request')
-    sub = root['sub']
+    root = MyResource("request")
+    sub = root["sub"]
     assert isinstance(sub, SubResource)
-    assert sub.request == 'request'
-    assert sub.__name__ == 'sub'
+    assert sub.request == "request"
+    assert sub.__name__ == "sub"
     assert sub.__parent__ is root
-    assert sub.foo == 'bar'
+    assert sub.foo == "bar"
 
 
 def test_getattr():
@@ -69,25 +70,25 @@ def test_getattr():
         pass
 
     class MyResource(Resource):
-        subfoo = 'subbar'
+        subfoo = "subbar"
 
         @property
         def prop(self):
-            return 'myprop'
+            return "myprop"
 
-    parent = MyResource('request')
-    child = SubResource('request', 'sub', parent, foo='bar')
-    grandchild = SubResource('request', 'sub', child)
+    parent = MyResource("request")
+    child = SubResource("request", "sub", parent, foo="bar")
+    grandchild = SubResource("request", "sub", child)
     with pytest.raises(AttributeError):
         assert parent.foo
-    assert parent.subfoo == 'subbar'
-    assert parent.prop == 'myprop'
-    assert child.foo == 'bar'
-    assert child.subfoo == 'subbar'
-    assert child.prop == 'myprop'
-    assert grandchild.foo == 'bar'
-    assert grandchild.subfoo == 'subbar'
-    assert grandchild.prop == 'myprop'
+    assert parent.subfoo == "subbar"
+    assert parent.prop == "myprop"
+    assert child.foo == "bar"
+    assert child.subfoo == "subbar"
+    assert child.prop == "myprop"
+    assert grandchild.foo == "bar"
+    assert grandchild.subfoo == "subbar"
+    assert grandchild.prop == "myprop"
 
 
 # Integration tests
@@ -105,17 +106,17 @@ def app():
             if int_key not in range(10):
                 raise KeyError
 
-            return Widget, {'widget_id': int_key}
+            return Widget, {"widget_id": int_key}
 
     class Widget(Resource):
         @property
         def widget(self):
             # Perhaps a DB lookup
-            return 'mywidget:{}'.format(self.widget_id)
+            return "mywidget:{}".format(self.widget_id)
 
     class Root(Resource):
         __children__ = {
-            'widget': WidgetContainer,
+            "widget": WidgetContainer,
         }
 
     def list_widgets(context, request):
@@ -123,7 +124,7 @@ def app():
         for widget_id in [1, 2, 5]:
             urls.append(request.resource_path(context[widget_id]))
         return {
-            'widget_urls': urls,
+            "widget_urls": urls,
         }
 
     def get_widget_id(context, request):
@@ -135,35 +136,36 @@ def app():
 
     with Configurator(settings={}) as config:
         config.set_root_factory(Root)
-        config.add_view(list_widgets, context=WidgetContainer, renderer='json')
-        config.add_view(get_widget, context=Widget, renderer='string')
-        config.add_view(get_widget_id, context=Widget, name='id',
-                        renderer='string')
+        config.add_view(list_widgets, context=WidgetContainer, renderer="json")
+        config.add_view(get_widget, context=Widget, renderer="string")
+        config.add_view(
+            get_widget_id, context=Widget, name="id", renderer="string"
+        )
     return _TestApp(config.make_wsgi_app())
 
 
 def test_get_widget_id(app):
-    assert app.get('/widget/5/id').text == '5'
+    assert app.get("/widget/5/id").text == "5"
 
 
 def test_get_widget(app):
-    assert app.get('/widget/5/').text == 'mywidget:5'
+    assert app.get("/widget/5/").text == "mywidget:5"
 
 
 def test_get_widget_not_found(app):
-    assert app.get('/widget/15/', status=404)
+    assert app.get("/widget/15/", status=404)
 
 
 def test_get_widget_bad_id(app):
-    assert app.get('/widget/not-an-int/', status=404)
+    assert app.get("/widget/not-an-int/", status=404)
 
 
 def test_get_widget_list(app):
-    assert app.get('/widget/').json == {
-        'widget_urls': [
-            '/widget/1/',
-            '/widget/2/',
-            '/widget/5/',
+    assert app.get("/widget/").json == {
+        "widget_urls": [
+            "/widget/1/",
+            "/widget/2/",
+            "/widget/5/",
         ],
     }
 
@@ -173,7 +175,7 @@ def test_get_widget_list(app):
 
 class ResolverRoot(Resource):
     __children__ = {
-        'foo': '.ToResolve',
+        "foo": ".ToResolve",
     }
 
 
@@ -182,14 +184,13 @@ class ToResolve(Resource):
 
 
 def test_resolve_children():
-
     def hello(context, request):
-        return 'Hello world!'
+        return "Hello world!"
 
     with Configurator(settings={}) as config:
         config.set_root_factory(ResolverRoot)
-        config.add_view(hello, context=ToResolve, renderer='string')
+        config.add_view(hello, context=ToResolve, renderer="string")
         config.scan()
     app = _TestApp(config.make_wsgi_app())
 
-    assert app.get('/foo').text == 'Hello world!'
+    assert app.get("/foo").text == "Hello world!"
